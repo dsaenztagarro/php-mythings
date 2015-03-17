d0ae9b96e4b35825d3e8d8c19cbcd6463ee6e3f5
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-03-17 16:13:04 +0100

    Adding Zend\Db\Sql and Zend\Stdlib\Hydrator

--- module/Blog/src/Blog/Mapper/ZendDbSqlMapper.php
+++ module/Blog/src/Blog/Mapper/ZendDbSqlMapper.php
@@ -17,25 +17,54 @@ class ZendDbSqlMapper implements PostMapperInterface
     protected $dbAdapter;
 
     /**
-     * @param AdapterInterface  $dbAdapter
+     * @var \Zend\Stdlib\Hydrator\HydratorInterface
      */
-    public function __construct(AdapterInterface $dbAdapter)
-    {
-        $this->dbAdapter = $dbAdapter;
+    protected $hydrator;
+
+    /**
+     * @var \Blog\Model\PostInterface
+     */
+    protected $postPrototype;
+
+    /**
+     * @param AdapterInterface  $dbAdapter
+     * @param HydratorInterface $hydrator
+     * @param PostInterface    $postPrototype
+     */
+    public function __construct(
+        AdapterInterface $dbAdapter,
+        HydratorInterface $hydrator,
+        PostInterface $postPrototype
+    ) {
+        $this->dbAdapter      = $dbAdapter;
+        $this->hydrator       = $hydrator;
+        $this->postPrototype  = $postPrototype;
     }
 
     /**
      * @param int|string $id
      *
-     * @return \Blog\Entity\PostInterface
+     * @return PostInterface
      * @throws \InvalidArgumentException
      */
     public function find($id)
     {
+        $sql    = new Sql($this->dbAdapter);
+        $select = $sql->select('posts');
+        $select->where(array('id = ?' => $id));
+
+        $stmt   = $sql->prepareStatementForSqlObject($select);
+        $result = $stmt->execute();
+
+        if ($result instanceof ResultInterface && $result->isQueryResult() && $result->getAffectedRows()) {
+            return $this->hydrator->hydrate($result->current(), $this->postPrototype);
+        }
+
+        throw new \InvalidArgumentException("Blog with given ID:{$id} not found.");
     }
 
     /**
-     * @return array|\Blog\Entity\PostInterface[]
+     * @return array|PostInterface[]
      */
     public function findAll()
     {
@@ -45,7 +74,12 @@ class ZendDbSqlMapper implements PostMapperInterface
         $stmt   = $sql->prepareStatementForSqlObject($select);
         $result = $stmt->execute();
 
-        // return $result;
-		\Zend\Debug\Debug::dump($result);die();
+        if ($result instanceof ResultInterface && $result->isQueryResult()) {
+            $resultSet = new HydratingResultSet($this->hydrator, $this->postPrototype);
+
+            return $resultSet->initialize($result);
+        }
+
+        return array();
     }
 }
